8e6fdd91bbc51319e0745ace3720c7fb9622eeeb
aeos161 <aeos161@protonmail.com>
2022-03-31 09:07:42 -0400

    Improve the Test Helper for Word Creation
    Signed-off-by: aeos161 <aeos161@protonmail.com>

--- spec/support/helpers/word_helper.rb
+++ spec/support/helpers/word_helper.rb
@@ -6,7 +6,7 @@ module WordHelper
 
   def create_word(text)
     alphabet = Primus::GematriaPrimus.build
-    tks = Array(text).map { |tk| alphabet.find_by(letter: tk) }
+    tks = Array(text).map(&:downcase).map { |tk| alphabet.find_by(letter: tk) }
     Primus::Word.new(tokens: tks)
   end
 end
