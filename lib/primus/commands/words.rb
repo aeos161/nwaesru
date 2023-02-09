9957bf6abfa73b4112451582bbb4b6d49b1b0367
aeos161 <aeos161@protonmail.com>
2023-02-08 22:37:11 -0500

    Refactor the Words Command

--- lib/primus/commands/words.rb
+++ lib/primus/commands/words.rb
@@ -4,28 +4,42 @@ class Primus::Commands::Words < Primus::Commands::SubCommandBase
   option :only_english_words, type: :boolean
   option :only_prime_sums, type: :boolean
   def generate
-    number_of_characters = options[:number_of_characters].to_i
+    generate_candidate_strings
+    limit_to_prime_sums_if_applicable
+    expand_strings_to_words
+    limit_to_english_words_if_applicable
+    print_results
+  end
+
+  protected
+
+  attr_reader :candidates
+
+  def generate_candidate_strings
     gp = Primus::GematriaPrimus.instance
-    candidates = gp.generate_words(number_of_characters: number_of_characters)
+    number_of_characters = options[:number_of_characters].to_i
+    @candidates = gp.generate_words(number_of_characters: number_of_characters)
+  end
 
-    if options[:only_prime_sums]
-      candidates = candidates.select { |word| word.sum.prime? }
-    end
+  def limit_to_prime_sums_if_applicable
+    return candidates unless options[:only_prime_sums]
+    @candidates = candidates.select { |word| word.sum.prime? }
+  end
 
-    candidates = candidates.flat_map do |word|
+  def expand_strings_to_words
+    @candidates = candidates.flat_map do |word|
       expander = Primus::Word::Expander.new(method: :letter)
       expander.visit_word(word)
       expander.results
-    end
+    end.uniq
+  end
 
-    if options[:only_english_words]
-      Linguo.api_key = ENV["LINGUO_API_KEY"]
-      # TODO use language detection api
-    end
+  def limit_to_english_words_if_applicable
+    # TODO: use oxford dictionary api
+  end
 
-    candidates = candidates.uniq
+  def print_results
     puts "Number of results: #{candidates.size}"
-
     candidates.each do |word|
       puts word.to_s
     end
